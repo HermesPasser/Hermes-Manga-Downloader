60aab44eadd5cd97b6bb3d6171d88ae9ece2bced
Hermes Passer <dsldouglas@hotmail.com>
2017-08-06 16:50:19 -0300

    Main logic

--- med/MainWindow.xaml.vb
+++ med/MainWindow.xaml.vb
@@ -4,18 +4,38 @@ Imports System.IO
 
 Class MainWindow
     Dim filepath As String
+    Dim errorMsg As String
 
-    Private Sub init(sender As Object, e As EventArgs)
+    Private Sub Init(sender As Object, e As EventArgs)
         filepath = ""
         textManga.Text = ""
         textChapter.Text = ""
         cbLanguage.SelectedIndex = 0
+        errorMsg = "This manga is not available or is licensed in your region, make sure you entered the name, chapter and language correctly."
     End Sub
 
+#Region "Menu"
+    Private Sub exit_Click(sender As Object, e As RoutedEventArgs)
+        System.Windows.Application.Current.Shutdown()
+    End Sub
+
+    Private Sub mfd_Click(sender As Object, e As RoutedEventArgs)
+        System.Diagnostics.Process.Start("https://hermespasser.github.io/pages/mangafoxdownloader.html")
+    End Sub
+
+    Private Sub mhd_Click(sender As Object, e As RoutedEventArgs)
+        System.Diagnostics.Process.Start("https://github.com/HermesPasser/Manga-Host-Downloader")
+    End Sub
+
+    Private Sub about_Click(sender As Object, e As RoutedEventArgs)
+        MessageBox.Show("Hermes Manga Downloader 0.1" + Environment.NewLine + "by Hermes Passer (gladiocitrico.blogspot.com / hermespasser@gmail.com)" + Environment.NewLine + "Github: HermesPasser/Hermes-Manga-Downloader")
+    End Sub
+#End Region
+
     Private Sub btnPath_Click(sender As Object, e As RoutedEventArgs)
-        Dim dialog As OpenFileDialog = New OpenFileDialog()
+        Dim dialog As FolderBrowserDialog = New FolderBrowserDialog()
         If dialog.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
-            filepath = dialog.FileName
+            filepath = dialog.SelectedPath + "\"
         End If
     End Sub
 
@@ -27,29 +47,43 @@ Class MainWindow
             Return
         End If
 
+        btnDownload.IsEnabled = False
+        btnDownload.Content = "Downloading..."
         Dim cb As String = IIf(cbLanguage.SelectedIndex = 0, "en", "it")
-        Dim str As String = "http://www.mangaeden.com/en/" + cb + "-directory/" + textManga.Text + "/" + textChapter.Text + "/1/"
+        Dim str As String = "http://www.mangaeden.com/en/" + cb + "-manga/" + textManga.Text + "/" + textChapter.Text + "/1/"
         Dim t As String() = GetTextOfImageLinks(GetHTML(str))
 
         If t IsNot Nothing Then
             t = GetImageLinksInTheText(t)
             DownloadImages(t, filepath + textManga.Text + "_c" + textChapter.Text)
+            MessageBox.Show("Download Completed.")
         Else
-            MessageBox.Show("This manga is not available or is licensed in your region, make sure you entered the name, chapter and language correctly.")
+            MessageBox.Show(errorMsg)
         End If
+        btnDownload.IsEnabled = True
+        btnDownload.Content = "Download"
     End Sub
 
     Private Function GetHTML(url As String) As String
-        Dim res As WebResponse = (WebRequest.Create(url)).GetResponse()
-        Dim reader As StreamReader = New StreamReader(res.GetResponseStream())
-        Dim result As String = reader.ReadToEnd()
-        reader.Close()
-        res.Close()
+        Dim result As String = Nothing
+        Try
+            Dim res As WebResponse = (WebRequest.Create(url)).GetResponse()
+            Dim reader As StreamReader = New StreamReader(res.GetResponseStream())
+            result = reader.ReadToEnd()
+            reader.Close()
+            res.Close()
+        Catch e As WebException
+            '
+        End Try
         Return result
     End Function
 
     'Get the line in the html that contains the image links
     Private Function GetTextOfImageLinks(htmlText As String) As String()
+        If htmlText Is Nothing Then
+            Return Nothing
+        End If
+
         Dim htmLines As String() = htmlText.Split(Environment.NewLine.ToCharArray())
 
         For Each line In htmLines
@@ -88,5 +122,4 @@ Class MainWindow
             MessageBox.Show("link: " + link + Environment.NewLine + "filename: " + filename + Environment.NewLine + "Error: " + e.ToString())
         End Try
     End Sub
-
 End Class
