7b318633c98f31932846cb7eb03398c5f1d0b8ac
Hermes Passer <dsldouglas@hotmail.com>
2019-05-31 17:28:34 -0300

    update version

--- Shared.vb
+++ Shared.vb
@@ -2,7 +2,7 @@
 Imports HUtils
 
 Public Class [Shared]
-    Public Shared appName As String = "Hermes Manga Downloader 0.4"
+    Public Shared appName As String = "Hermes Manga Downloader 0.5"
     Public Shared emptyFieldMsg As String = "There are empty fields, did you complete them?"
     Public Shared errorDefault As String = "This manga is not available, make sure you entered the name and chapter correctly."
     Public Shared errorMsgEden As String = "This manga is not available or is licensed in your region, make sure you entered the name, chapter and language correctly."
